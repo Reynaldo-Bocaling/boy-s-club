63e80f995d55786640cc01cd9093ee0da91a3d72
Reynaldo <reynaldobocaling@gmail.com>
2024-07-26 12:19:11 +0800

    upload proj

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,12 @@ import nft2 from "/img/NFT2.png";
 import nft3 from "/img/NFT3.png";
 import nft4 from "/img/NFT4.png";
 
+import h1 from "/img/h1.png";
+import h2 from "/img/h2.png";
+import h3 from "/img/h3.png";
+import h4 from "/img/h4.png";
+import h5 from "/img/h4.png";
+
 export const navlinks = [
   {
     label: "Members",
@@ -106,3 +112,5 @@ export const faq = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam eos assumenda minus delectus dolor blanditiis fuga sequi sunt, eum vel!",
   },
 ];
+
+export const h_img = [nft1, nft2, nft3, nft4];
--- src/view/Hero.jsx
+++ src/view/Hero.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import SlideText from "../components/SlideText";
+import { h_img } from "../constants";
 
 const Hero = () => {
   return (
     <section id="hero " className="relative mt-14  z-[98]">
       <div className="heroLine_bg relative max-w-7xl  w-full mx-auto h-[400px] md:h-[550px] flex pt-12 border-[2px]  pl-9">
+        <div className="max-w-md w-full  mx-auto bg-[#02d5ff] py-4 absolute left-1/2 -translate-x-1/2 -translate-y-20 flexCenter gap-7">
+          {h_img.map((item, index) => (
+            <div
+              key={index}
+              className="relative w-[70px] h-[70px] bg-white rounded-full -translate-y-5 overflow-hidden border-[3px] border-blue-500"
+            >
+              <img src={item} className="w-full absolute bottom-0 left-0" />
+            </div>
+          ))}
+        </div>
         <div className="w-1/2 z-[98]">
           <h3 className="text-xl font-bold text-white mb-4">Please Join</h3>
           <h1 className="title text-5xl md:text-7xl font-bold text-white">
